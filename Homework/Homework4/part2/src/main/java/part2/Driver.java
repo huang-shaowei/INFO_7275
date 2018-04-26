d3ad0d43ee49cc7a69e0bf24345c6c3876ded2a2
huang-shaowei <huang.shaow@husky.neu.edu>
2018-04-26 13:14:02 -0400

    Add files via upload

---
+++ Homework/Homework4/part2/src/main/java/part2/Driver.java
@@ -0,0 +1,69 @@
+package part2;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+
+public class Driver {
+
+	public static void main(String[] args) throws Exception {
+		if (args.length != 2) {
+			System.err.println("Usage: MaxSubmittedCharge <input path> <output path>");
+			System.exit(-1);
+		}
+
+		Path inputPath = new Path(args[0]);
+		Path outputDir = new Path(args[1]);
+
+		// Create configuration
+		Configuration conf = new Configuration(true);
+
+		// Create job		
+		//Job job = new Job(conf, "SecondarySort");
+		Job job = Job.getInstance(conf);
+		job.setJarByClass(DateMapper.class);
+		
+
+		// Setup MapReduce
+		job.setMapperClass(DateMapper.class);
+		job.setPartitionerClass(DatePartitioner.class);
+		DatePartitioner.setAcessDate(job, 2015);
+		job.setReducerClass(DateReducer.class);
+		job.setNumReduceTasks(12);
+
+		// Specify key / value
+	
+		job.setMapOutputKeyClass(IntWritable.class);
+		job.setMapOutputValueClass(Text.class);
+		
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(NullWritable.class);
+		// Input
+		FileInputFormat.addInputPath(job, inputPath);
+		job.setInputFormatClass(TextInputFormat.class);
+
+		// Output
+		FileOutputFormat.setOutputPath(job, outputDir);
+
+		// Delete output if exists
+		FileSystem hdfs = FileSystem.get(conf);
+		if (hdfs.exists(outputDir))
+			hdfs.delete(outputDir, true);
+
+		// Execute job
+		int code = job.waitForCompletion(true) ? 0 : 1;
+		System.exit(code);
+		
+		
+
+	}
+
+}
